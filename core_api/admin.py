2364ade491fbcd711fb94fd9baf158d808c7c581
Kim-Talha <talhahabib319@gmail.com>
2023-10-01 00:08:58 -0700

    create product category user-profile order order-item models

--- core_api/admin.py
+++ core_api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import UserProfile, Product, Category, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'phone_number', 'created_at', 'updated_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'created_at', 'updated_at')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ordered_date', 'total_price', 'is_ordered', 'created_at', 'updated_at')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity', 'created_at', 'updated_at')
